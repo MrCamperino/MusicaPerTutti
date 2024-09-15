ea303392c70967ac3dec6abb03cf4ee332aae825
Gabriele Deda <130688764+PacciaLover69@users.noreply.github.com>
2024-09-15 16:30:26 +0200

    push

---
+++ index.js
@@ -0,0 +1,9 @@
+contattiStrisciolina = document.getElementById("contatti_strisciolina"); 
+
+window.addEventListener('resize', function(event) {
+    if (window.innerWidth <= 870) {
+        contattiStrisciolina.remove();
+    } else if (window.innerWidth > 870) {
+        document.getElementById("strisciolina").appendChild(contattiStrisciolina);
+    }
+});
